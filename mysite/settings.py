454a10ebe29f847675b68fc09973f714bbe6c738
anas <ansmuhd01@gmail.com>
2023-11-02 23:19:04 +0530

    simple version completed commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
 # Application definition
 
--- restaurent/urls.py
+++ restaurent/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home')
+    path('', views.MenuList.as_view(), name='home'),
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name='item')
 ]
+
