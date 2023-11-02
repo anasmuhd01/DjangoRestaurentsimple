811543432b66c856c818c754d4974bb6eff37514
anas <ansmuhd01@gmail.com>
2023-11-02 17:18:29 +0530

    views.py

--- restaurent/views.py
+++ restaurent/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from  .models import Item
+from  .models import Item, MEAL_TYPE
 
 # Create your views here.
 
@@ -8,8 +8,9 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {"meal":"Pizza"}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
 class MenuItemDetail(generic.DetailView):
     model = Item
